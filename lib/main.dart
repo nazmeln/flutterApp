ceec3c7e7c7c96fbf71b6787868dc367728ef3bc
Nazarii Melnyk <nazik100378@gmail.com>
2021-06-25 11:36:00 +0300

    create separate widget and use statefullWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,33 @@
 import 'package:flutter/material.dart';
 
+import './questions.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   @override // decorator provided by Flutter (for code style and we overriding it with our implementation)
+
+  int _titleIndex = 0;
+
+  void printClick() {
+    // setState(() => {_titleIndex += 1});
+    print('onButton clicked');
+  }
+
+  List<String> titles = [
+    'Title 1',
+    'Title 2',
+    'Title 3',
+  ];
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
@@ -12,7 +36,17 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("My First App"),
         ), // builds appBar
-        body: Text('This is my default Text'), // builds body
+        body: Center(
+          child: Column(
+            children: [
+              Questions(titles[_titleIndex]),
+              ElevatedButton(
+                  onPressed: printClick, child: Text('ElevatedButton')),
+              ElevatedButton(
+                  onPressed: printClick, child: Text('ElevatedButton'))
+            ],
+          ),
+        ), // builds body
       ),
     );
   }
---
+++ lib/questions.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Questions extends StatelessWidget {
+  final String questionText;
+
+  Questions(this.questionText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(questionText),
+    );
+  }
+}
